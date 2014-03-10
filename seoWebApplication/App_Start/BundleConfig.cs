a10c252481282007dde39c005ca99eab8d0d4e95
Ty Moodley <ty.moodley@gmail.com>
2014-03-09 22:13:59 -0400

    upgrades for MVC update.

---
+++ seoWebApplication/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace seoWebApplication
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+              // The Kendo JavaScript bundle
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                    "~/Scripts/kendo/2013.1.319/kendo.web.min.js", // or kendo.all.* if you want to use Kendo UI Web and Kendo UI DataViz
+                    "~/Scripts/kendo/2013.1.319/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                 "~/Scripts/jquery-{version}.js")); 
+
+            // The Kendo CSS bundle
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                    "~/Content/kendo.common.*",
+                    "~/Content/kendo.default.*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+ 
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            // Clear all items from the default ignore list to allow minified CSS and JavaScript files to be included in debug mode
+            bundles.IgnoreList.Clear();
+
+            // Add back some of the default ignore list rules
+            bundles.IgnoreList.Ignore("*.intellisense.js");
+            bundles.IgnoreList.Ignore("*-vsdoc.js");
+            bundles.IgnoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+        }
+    }
+}
---
+++ seoWebApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace seoWebApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+          name: "StoreDepartment",
+          url: "store/{storeid}/d/{id}/{name}/",
+          defaults: new { controller = "Product", action = "Departments", storeid = UrlParameter.Optional, id = UrlParameter.Optional, name = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+        name: "StoreCategory",
+        url: "store/{storeid}/c/{id}/{name}/",
+        defaults: new { controller = "Product", action = "Categories", storeid = UrlParameter.Optional, id = UrlParameter.Optional, name = UrlParameter.Optional }
+      );
+
+            routes.MapRoute(
+             name: "StoreProduct",
+             url: "store/{storeid}/product/{id}/{name}/",
+             defaults: new { controller = "Product", action = "Display", storeid = UrlParameter.Optional, id = UrlParameter.Optional, name = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "Store",
+              url: "store/{id}/{name}/",
+              defaults: new { controller = "webstore", action = "store", id = UrlParameter.Optional, name = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+           name: "CategoryType",
+           url: "ct/{id}/{productName}/",
+           defaults: new { controller = "CategoryType", action = "Types", id = UrlParameter.Optional, productName = UrlParameter.Optional }
+         );
+
+            routes.MapRoute(
+                     name: "Default",
+                     url: "{controller}/{action}/{id}",
+                     defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                 );
+
+           
+
+            
+
+      
+        }
+    }
+}
